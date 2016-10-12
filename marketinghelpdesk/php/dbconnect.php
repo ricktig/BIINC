410f18ab520f200d3e24e4be9d66dd068b9bcfaf
Rick Rose <ricktig@q.com>
2016-10-12 10:31:23 -0600

    Update dbconnect.php

--- marketinghelpdesk/php/dbconnect.php
+++ marketinghelpdesk/php/dbconnect.php
@@ -5,8 +5,7 @@ ini_set('display_errors', '1');
 //create new PDO db object
 try {
 	$user = 'marketinghdadmin';
-	//$pw = 'M!@12Z$ki%G';
-	$pw = 'asdf1234';
+	$pw = 'mydbpassword';//place correct password here
 	$db = new PDO('mysql:host=localhost;dbname=marketinghd;charset=utf8', $user, $pw);
 } catch (PDOException $e) {
 	echo $e->getMessage();
