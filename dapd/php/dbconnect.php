f65f774176271a8196356047f678977f5b9af751
Rick Rose <ricktig@q.com>
2016-10-12 10:20:31 -0600

    Add files via upload

---
+++ dapd/php/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+
+//dapd db connect
+$host = 'localhost';
+$database = 'dapd';
+$user = 'dapd';
+$pw = 'ak$Jpo909!';
+$db = mysql_connect($host,$user,$pw) or die( 'Error'. mysql_error() );
+?>
