6c8237d92eeec01657307af6ed15d56e2f31284a
Rick Rose <ricktig@q.com>
2016-10-12 10:18:49 -0600

    Update dbconnect.php

--- techforum/php/dbconnect.php
+++ techforum/php/dbconnect.php
@@ -3,7 +3,7 @@
 //create new PDO db object
 try {
 	$user = 'techforumadmin';
-	$pw = '8hQU*%g6u$';
+	$pw = 'mypasswordgoeshere'; //put in correct pw here
 	$db = new PDO('mysql:host=localhost;dbname=techforum;charset=utf8', $user, $pw);
 } catch (PDOException $e) {
 	echo $e->getMessage();
